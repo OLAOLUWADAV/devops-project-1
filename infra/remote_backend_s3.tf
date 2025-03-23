e2f7cc6fbc5867ef0473a1214abb950d7fb0efed
OLAOLUWADAV <148901308+OLAOLUWADAV@users.noreply.github.com>
2025-03-23 03:07:01 +0100

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "iac-ecom-terraform-state-123456"
+    bucket = "dev-proj1jenkins-remote-state-bucket-123456"
     key    = "devops-project-1/terraform.tfstate"
     region = "ap-south-1"
   }
